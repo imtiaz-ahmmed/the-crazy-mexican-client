1c4c70b88205bb37322a8562634c29a7a97cbf2e
imtiaz-ahmmed <imtiazahmmed00@gmail.com>
2023-05-02 17:41:53 +0600

    spinner is added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../pages/Shared/Header/Header";
 import Footer from "../pages/Shared/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import RingLoader from "react-spinners/RingLoader";
 
 const Main = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
     <div>
       <Header></Header>
-      <Outlet></Outlet>
+      {loading ? (
+        <div className="spinner-container bg-slate-400 h-screen mx-auto">
+          <RingLoader />
+        </div>
+      ) : (
+        <Outlet></Outlet>
+      )}
       <Footer></Footer>
     </div>
   );
